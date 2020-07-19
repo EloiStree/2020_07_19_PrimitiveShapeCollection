b8dbccd8c53b496caab202d9a24b9264da118761
Eloi Stree <eloistree@gmail.com>
2020-07-19 23:46:35 +0200

    Save: 07/19/2020 11:46 PM

---
+++ Editor/DisposeChildrenInEditor.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DisposeChildrenInEditor : MonoBehaviour
+{
+    [Header("In Line")]
+    public int m_distanceOfObjectsInLine = 2;
+
+
+    [ContextMenu("Put Children In Line")]
+    public void SetInLine() {
+
+        for (int i = 0; i < transform.childCount; i++)
+        {
+            transform.GetChild(i).localPosition = Vector3.right * i* m_distanceOfObjectsInLine;
+        }
+    }
+}
